3f7da9f5f6225646c3bd8a0812a5d6f4b8ffe6cd
Tyler Crawford <122412195+TenTides@users.noreply.github.com>
2023-02-07 03:52:32 +0000

    Added tables and constants, initalized lex parse.

--- scanner.c
+++ scanner.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h> 
-#include <limits.h>
 #include <string.h>
+#define  NORW      15       // number of reserved words (not very useful)
+#define  IMAX   32767       // maximum integer value 
+#define  CMAX      11       // maximum number of chars for idents 
+#define  STRMAX   256       // maximum length of strings (when to use this?)
+
+char* word[] = {"null", "begin", "call", "const", "do", "else", "end", "if",
+                 "odd", "procedure", "read", "then", "var", "while", "write"}; 
+//Symbol table isn't in order of ascii, cannot be used with a hash search method yet.
+char  symbolTable[] = {'a','b','c','d','e','f','g','h','i','j','k','l' ,'m' ,'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+                        'w', 'x', 'y', 'z', '0', '1', '2','3', '4', '5', '6', '7', '8', '9',' ', '+', '-', '*', '/', '<', 
+                        '=', '>', ':','.' , ',' , ';' };   
+//Symbols which are essentially breakpoints and line enders // is not in here, a lookahead check is necessary for that one
+char specialTerminalSymbols[] = {'+', '-', '*', '/', '<', '=', '>', ':','.' , ',' , ';'};    
+                            
+              
+char* lexicalParse(char* codeLine);
+              
 typedef enum {  
-    skipsym = 1, 
+    nulsym = 1, //was skipsys, but skip isn't in PL/0, this must be null
     identsym = 2, 
     numbersym = 3, 
     plussym = 4 , 
@@ -37,7 +53,13 @@ typedef enum {
     readsym = 32, 
     elsesym = 33  
 }token_type; 
-
+//Intent is to iterate character by character from a given
+//line of code to produce the token version of that code 
+//and thereafter return that string
+char* lexicalParse(char* codeLine)
+{
+    return codeLine; //temp return statement
+}
 int main(int argc, char *argv[])
 {
     FILE *fp;
@@ -52,19 +74,27 @@ int main(int argc, char *argv[])
       }
     }
     fclose(fp);
-    char* codePL = malloc(sizeof(char)*LINE_MAX*numLines);
-    codePL[0] = '\0';
+    char* codePL = malloc(sizeof(char)*STRMAX*numLines);
+    codePL[0] = '\0'; // Must be set to the first index to allow for smooth cats
+
     fp = fopen(argv[1], "r");
     while(1)
     {
-        char* line = malloc(sizeof(char)*LINE_MAX);
+        char* line = malloc(sizeof(char)*STRMAX);
         if(fscanf(fp, "%[^\n]", line) == EOF)
         {
             break;
         }
         line = realloc(line,sizeof(char)*strlen(line));
-        strcat(codePL,line);
+        line = lexicalParse(line);
+        if(strlen(line) >= 0)// Because a line, or a part of it, can be a comment
+        { 
+            strcat(codePL,line);
+        } 
+        else
+        {
+            break; // an error was encountered prematurely
+        }
         free(line);
-    }
-     
+    }   
 }
